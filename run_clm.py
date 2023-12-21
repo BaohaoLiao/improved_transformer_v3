44850dfa7b6c97c8982513a41e85dd64841240a8
baohao <liaobaohao@gmail.com>
2023-12-21 15:42:00 +0100

    first commit

--- run_clm.py
+++ run_clm.py
@@ -230,15 +230,8 @@ class DataTrainingArguments:
         if self.streaming:
             require_version("datasets>=2.0.0", "The streaming feature requires `datasets>=2.0.0`")
 
-        if self.dataset_name is None and self.train_file is None and self.validation_file is None:
-            raise ValueError("Need either a dataset name or a training/validation file.")
-        else:
-            if self.train_file is not None:
-                extension = self.train_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], "`train_file` should be a csv, a json or a txt file."
-            if self.validation_file is not None:
-                extension = self.validation_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], "`validation_file` should be a csv, a json or a txt file."
+        if self.dataset_name is None :
+            raise ValueError("Need either a dataset name.")
 
 
 def main():
