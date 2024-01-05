89f9f76bfe398722dd846f7f7291f50a6f7a56b0
baohao <liaobaohao@gmail.com>
2024-01-05 12:12:02 +0100

    quantize registered tokens model

--- models/opt_for_causal_lm.py
+++ models/opt_for_causal_lm.py
@@ -26,6 +26,9 @@ class OPTForCausalLMWithExtras(OPTForCausalLM):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
+        print("input_ids:", input_ids)
+        print("attention_mask:", attention_mask)
+        print("labels:", labels)
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
