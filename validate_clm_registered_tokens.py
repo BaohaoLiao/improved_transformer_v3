c20b103b24c360c11f9f9a418ff726be376c5046
baohao <liaobaohao@gmail.com>
2024-01-05 12:06:51 +0100

    quantize registered tokens model

--- validate_clm_registered_tokens.py
+++ validate_clm_registered_tokens.py
@@ -439,7 +439,7 @@ def main():
 
         def add_registered_tokens(examples):
             result = {}
-            print(examples)
+            print(examples.keys())
             for k, examples in examples.items():
                 new_examples = []
                 for example in examples:
