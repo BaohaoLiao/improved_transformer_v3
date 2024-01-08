56c034115b309bf1d07a14bcd1a9defff201514f
baohao <liaobaohao@gmail.com>
2024-01-08 16:49:45 +0100

    modify run_mlm.py

--- validate_clm_registered_tokens_v3.py
+++ validate_clm_registered_tokens_v3.py
@@ -458,7 +458,7 @@ def main():
             return result
 
         # speed up the data processing
-        #lm_datasets["train"] = lm_datasets["train"].shuffle(seed=training_args.seed).select(range(1000))
+        lm_datasets["train"] = lm_datasets["train"].shuffle(seed=training_args.seed).select(range(1000))
         #lm_datasets["validation"] = lm_datasets["validation"].select(range(1000))
         with training_args.main_process_first(desc="adding registered tokens"):
             if not data_args.streaming:
